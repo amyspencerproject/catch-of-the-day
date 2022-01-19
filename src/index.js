6569aebcce4fedb4b4dddbac8a97a12711d89414
amyspencerproject <amy@spencerproject.com>
2022-01-19 15:06:01 -0700

    Added first event handler for store selection by user

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Router from './components/Router';
-// import StorePicker from './components/StorePicker';
 import './css/style.css';
 
 
