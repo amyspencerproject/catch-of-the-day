cb8acf5e4b3861005d4b0695b8d9dcaa4ed9b063
amyspencerproject <amy@spencerproject.com>
2022-01-18 22:47:10 -0700

    Added routing via React Router and changed Header into stateless functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,7 @@
 import React from "react";
 
-class Header extends React.Component {
-    render() {
-        return (
+//stateless functional component can be used because no state in header only data passed in and JSX returned
+const Header = (props) => (
             <header className="top">
                 <h1>Catch 
                 <span className="ofThe"> 
@@ -11,12 +10,30 @@ class Header extends React.Component {
                 </span> 
                 Day</h1>
                 <h3 className="tagline">
-                    <span>Fresh Daily</span>
+                <span>{props.tagline}</span>
                 </h3>
             </header>
         )
-    }
-}
+
+
+//state functional component which uses this. and would handle more complex data flow than stateless
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>Catch 
+//                 <span className="ofThe"> 
+//                 <span className="of">Of</span>
+//                 <span className="the">The</span>
+//                 </span> 
+//                 Day</h1>
+//                 <h3 className="tagline">
+//                 <span>{this.props.tagline}</span>
+//                 </h3>
+//             </header>
+//         )
+//     }
+// }
 
 export default Header;
 
