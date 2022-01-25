d7c9db39346fd311dc69e1e7d2103e47e1b21842
amyspencerproject <amy@spencerproject.com>
2022-01-24 22:54:26 -0700

    Added state for fish samples and updated state into JSX

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends React.Component {
 
@@ -22,14 +24,25 @@ class App extends React.Component {
         })
     };
 
+    loadSampleFishes = () => {
+        this.setState({fishes: sampleFishes}) //this sampleFishes is what we called import of sample-fishes.js
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline = "Fresh Seafood Market" />
+                    <ul className="fishes"> 
+                        {Object.keys(this.state.fishes).map(key => 
+                        <Fish key={key} details={this.state.fishes[key]} />)} 
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish}/>
+                <Inventory 
+                    addFish={this.addFish} 
+                    loadSampleFishes={this.loadSampleFishes} 
+                />
             </div>
         );
     }
